f5105a54647be697a291816b511deb9bfba1290d
GabiOC <oconnor.gabriele@gmail.com>
2015-08-03 15:41:42 -0400

    all done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    species = []
+    self.each do |animal, location|
+    	# binding.pry
+    	arguments.each do |argument|
+    		if location == argument
+    			species << animal
+    		end
+    	end
+    end
+    species
   end
 end
